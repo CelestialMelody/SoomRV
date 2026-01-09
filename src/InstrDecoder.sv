cc7325f429f33cd984081125dad1171194d645f2
Clstilmldy <celestialmelodygo@gmail.com>
2026-01-09 13:10:30 +0800

    Fix: The INT_FSGNJ_S has been modified to have the BM_ prefix, and this can be found in Include.sv.

--- src/InstrDecoder.sv
+++ src/InstrDecoder.sv
@@ -971,12 +971,19 @@ always_comb begin
                                 5'b00100: begin
                                     uop.fu = FU_INT;
 
+                                    // FIXED
+                                    // ENABLE_FP in Config.sv
+                                    // Can't find definition of variable: 'INT_FSGNJN_S'
+                                    // But BM_FSGNJ?_S are defined in Include.sv
                                     if (i32.fp.rm == 3'b000)
-                                        uop.opcode = INT_FSGNJ_S;
+                                        // uop.opcode = INT_FSGNJ_S;
+                                        uop.opcode = BM_FSGNJ_S;
                                     else if (i32.fp.rm == 3'b001)
-                                        uop.opcode = INT_FSGNJN_S;
+                                        // uop.opcode = INT_FSGNJN_S;
+                                        uop.opcode = BM_FSGNJN_S;
                                     else if (i32.fp.rm == 3'b010)
-                                        uop.opcode = INT_FSGNJX_S;
+                                        // uop.opcode = INT_FSGNJX_S;
+                                        uop.opcode = BM_FSGNJX_S;
                                     else invalidEnc = 1;
                                 end
                                 5'b00101: begin
